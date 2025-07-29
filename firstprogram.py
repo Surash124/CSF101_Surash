23744d55c4db6195c727b9af5a5d9e5ac8797a10
Surash124 <surashu.124@gmail.com>
2025-07-29 14:08:05 +0530

    beginner

--- firstprogram.py
+++ firstprogram.py
@@ -1,2 +1,16 @@
-print("Hello SWE 2025")
-a=input("whats your name:")
+#print("Hello SWE 2025")
+a=input("Enter password")
+if a =="sage12":
+    print("Hello master, how can i help you")
+else:
+    print("Password incorrect")
+print("The output is...")
+print(" ")
+
+a=45
+b=67
+con=a+b
+print("The answer is :",con)
+
+for i in range(10):
+    print("Excelent")
